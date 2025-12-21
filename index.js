14e38626d6a873cd4af171a7410847b5472addad
Subroto Chanda Shuvo <subrotochandashuvo@gmail.com>
2025-12-21 16:13:24 +0600

    filtering in my request and send data for main Dashbord

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const verifyFBToken = async (req, res, next) => {
   try {
     const idToken = token.split(" ")[1];
     const decoded = await admin.auth().verifyIdToken(idToken);
-    console.log("decoded info", decoded);
+    // console.log("decoded info", decoded);
     req.decoded_email = decoded.email;
     next();
   } catch (error) {
@@ -54,7 +54,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
 
     const database = client.db("bloodlove");
@@ -93,12 +93,24 @@ async function run() {
       res.send(result);
     });
 
+    //Update Profile
+    app.patch("/users/update/profile", verifyFBToken, async (req, res) => {
+      const { email } = req.query;
+      const updatedProfile = req.body;
+
+      const query = { email: email };
+      const updateDoc = { $set: updatedProfile };
+
+      const result = await userCollections.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
     app.get("/users/role/:email", async (req, res) => {
       const { email } = req.params;
 
       const query = { email: email };
       const result = await userCollections.findOne(query);
-      console.log(result);
+      // console.log(result);
       res.send(result);
     });
 
@@ -110,29 +122,84 @@ async function run() {
       res.send(result);
     });
 
+    // All request
+    // app.get("/request", async (req, res) => {
+    //   const result = await requestsCollections.find().toArray();
+    //   console.log(result);
+    //   res.send(result);
+    // });
     app.get("/request", async (req, res) => {
-      const result = await requestsCollections.find().toArray();
-      console.log(result);
-      res.send(result);
+      const page = parseInt(req.query.page) || 0; // 0-indexed
+      const size = parseInt(req.query.size) || 9; // items per page
+
+      const totalRequests = await requestsCollections.countDocuments();
+
+      const requests = await requestsCollections
+        .find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+
+      res.send({
+        requests,
+        totalRequests,
+      });
     });
 
     // get my_request
     app.get("/my-request", verifyFBToken, async (req, res) => {
       const email = req.decoded_email;
+
+      const page = Number(req.query.page) || 0;
+      const size = Number(req.query.size) || 10;
+      const status = req.query.status;
+
+      // base query
       const query = {
         requesterEmail: email,
       };
-      const page = Number(req.query.page);
-      const size = Number(req.query.size);
+
+      if (status && status !== "all") {
+        query.donationStatus = status;
+      }
 
       const result = await requestsCollections
         .find(query)
+        .skip(page * size)
         .limit(size)
-        .skip(size * page)
         .toArray();
 
       const totalRequest = await requestsCollections.countDocuments(query);
-      res.send({ request: result, totalRequest: totalRequest });
+
+      res.send({
+        request: result,
+        totalRequest,
+      });
+    });
+
+    //main dashboard
+    app.get("/myRequest", async (req, res) => {
+      const { email, limit } = req.query;
+      console.log("hello");
+
+      console.log(email, limit);
+
+      const requests = await requestsCollections
+        .find({ requesterEmail: email })
+        .sort({ donationDate: -1 })
+        .limit(parseInt(limit) || 3)
+        .toArray();
+      console.log(requests);
+
+      res.send({ requests });
+    });
+
+    app.patch("/updateRequest/user/status", async (req, res) => {
+      const { requestId, status } = req.query;
+      const query = { _id: new ObjectId(requestId) };
+      const update = { $set: { donationStatus: status } };
+      const result = await requestsCollections.updateOne(query, update);
+      res.send(result);
     });
 
     //Payment
@@ -166,54 +233,48 @@ async function run() {
       res.send({ url: session.url });
     });
 
-
     // stripe er payment details DB te save
     app.post("/success-payment", async (req, res) => {
       const { session_id } = req.query;
-      const session = await stripe.checkout.sessions.retrieve(
-        session_id
-      );
-      console.log(session);
-      
-      const transactionId=session.payment_intent;
-      if(session.payment_status=='paid'){
-        const paymentInfo={
-          amount:session.amount_total/100,
+      const session = await stripe.checkout.sessions.retrieve(session_id);
+      // console.log(session);
+
+      const transactionId = session.payment_intent;
+      if (session.payment_status == "paid") {
+        const paymentInfo = {
+          amount: session.amount_total / 100,
           currency: session.currency,
           donorEmail: session.customer_email,
           transactionId,
           payment_status: session.payment_status,
           paidAt: new Date(),
-        }
-        const result = await paymentsCollections.insertOne(paymentInfo)
+        };
+        const result = await paymentsCollections.insertOne(paymentInfo);
         return res.send(result);
       }
     });
 
-    // search er Kaj\
-    app.get('/search-requests', async(req,res)=>{
-      const {bloodGroup, district, upazila} = req.query;
-      const query={}
-      if(!Query){
-        return
+    // search er Kaj
+    app.get("/search-requests", async (req, res) => {
+      const { bloodGroup, district, upazila } = req.query;
+      const query = {};
+      if (!Query) {
+        return;
       }
-      if(bloodGroup){
+      if (bloodGroup) {
         const fixed = bloodGroup.replace(/ /g, "+").trim();
-        query.bloodGroup=fixed;
+        query.blood = fixed;
       }
-      if(district){
-        query.recipientDistrict=district;
+      if (district) {
+        query.district = district;
       }
-      if(upazila){
-        query.recipientUpazila = upazila;
+      if (upazila) {
+        query.upazila = upazila;
       }
 
-      const result = await requestsCollections.find(query).toArray();
-      res.send(result)
-      
-
-    })
-
+      const result = await userCollections.find(query).toArray();
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
